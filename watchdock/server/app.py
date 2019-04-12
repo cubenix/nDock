bb2a643cf7bd760aeae25e0df10e64662886d870
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-04-12 17:05:28 +0530

    initializing server

---
+++ watchdock/server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/api/')
+def index():
+    return '<h1>Hello from WatchDock API Server</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
