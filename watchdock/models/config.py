deee2081a9ae3ff09ea3b3bc3a959cde5fad6943
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-04-15 13:11:21 +0530

    adding host and configuration model

---
+++ watchdock/models/config.py
@@ -0,0 +1,4 @@
+class Config:
+    """ Represents the object in config.json file. """
+    def __init__(self, hosts):
+        self.hosts = hosts
---
+++ watchdock/models/host.py
@@ -0,0 +1,5 @@
+class Host:
+    """ Represents the Docker Host to work with. """
+    def __init__(self, name, IP):
+        self.name = name
+        self.IP = IP
