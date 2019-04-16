d9c8340d846ab7f6a4863543e812dc180875de2f
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-04-16 10:42:07 +0530

    adding a few reusable constants

---
+++ watchdock/constants.py
@@ -0,0 +1,24 @@
+# default TCP port for Docker Host
+DEFAULT_PORT = "2375"
+
+
+# color codes for host representation; must NOT be less than number of hosts
+COLOR_HOSTS = [
+    "text-primary",
+    "text-success",
+    "text-info",
+    "text-warning",
+    "text-danger",
+    "text-secondary"
+    ]
+
+
+# color codes for chart background
+COLOR_BACKGROUND = [
+    "#4e73df",
+    "#1cc88a",
+    "#36b9cc",
+    "#f6c23e",
+    "#e74a3b",
+    "#858796",
+]
