681354cd8125c3d25c82a84ed88d4ee1713bf9ac
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-04-16 10:44:14 +0530

    get containers running on docker hosts

--- watchdock/app.py
+++ watchdock/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from config_manager import ConfigManager
+import services.host as host
+import constants
+
+
 app = Flask(__name__)
+config_manager = ConfigManager()
 
 
 @app.route('/')
@@ -7,7 +13,15 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/index.html')
 def index():
-    return render_template("index.html", title="Home")
+    hosts = config_manager.get_hosts()
+    host_containers = host.get_containers(hosts)
+
+    return render_template(
+        "index.html", title="Dashboard",
+        hosts=list(host_containers.keys()),
+        containers=list(host_containers.values()),
+        color_hosts=constants.COLOR_HOSTS,
+        color_background=constants.COLOR_BACKGROUND)
 
 
 if __name__ == '__main__':
---
+++ watchdock/services/host.py
@@ -0,0 +1,19 @@
+""" Helper to work with a Docker host. """
+from docker import DockerClient
+import constants
+
+
+def get_containers_for_hosts(hosts):
+    """Returns a dictionary with hostname and the number of containers in running state.
+
+    :param hosts: list of Docker hosts defined in config.json
+
+    Returns:
+        { 'hostname': containers-count }
+    """
+    host_containers = {}
+    for host in hosts:
+        client = DockerClient(f"tcp://{host.IP}:{constants.DEFAULT_PORT}")
+        host_containers[host.name] = len(client.containers.list())
+
+    return host_containers
