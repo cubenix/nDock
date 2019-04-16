a9fb117c80c0abd4e7bca18c8619f49be16a54d5
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-04-16 17:56:40 +0530

    getting container stats

--- watchdock/app.py
+++ watchdock/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config_manager import ConfigManager
 import services.renderer as renderer
+import services.host as host
 
 
 app = Flask(__name__)
@@ -13,6 +14,12 @@ config_manager = ConfigManager()
 @app.route('/index.html')
 def index():
     return renderer.render_dashboard(config_manager)
+
+
+@app.route('/stats')
+def stats():
+    host.get_stats()
+    return '<h1>Docker stats</h1>'
 
 
 if __name__ == '__main__':
--- watchdock/services/host.py
+++ watchdock/services/host.py
@@ -1,10 +1,13 @@
 """ Helper to work with a Docker host. """
-from docker import DockerClient
+import docker
+from models.host import Host
 import constants
 
 
 def get_containers(hosts):
-    """Returns a dictionary with hostname and the number of containers in running state.
+    """
+    Returns a dictionary with hostname and the number of
+    containers in running state.
 
     :param hosts: list of Docker hosts defined in config.json
 
@@ -13,7 +16,22 @@ def get_containers(hosts):
     """
     host_containers = {}
     for host in hosts:
-        client = DockerClient(f"tcp://{host.IP}:{constants.DEFAULT_PORT}")
+        client = __create_client(host)
         host_containers[host.name] = len(client.containers.list())
-
     return host_containers
+
+
+def get_stats():
+    host = Host(name="mglab-srv4", IP="172.27.127.134")
+    client = __create_low_level_client(host)
+    for container in client.containers():
+        print(next(client.stats(resource_id=container['Id'], decode=True)))
+
+
+def __create_client(host):
+    return docker.DockerClient(f"tcp://{host.IP}:{constants.DEFAULT_PORT}")
+
+
+def __create_low_level_client(host):
+    return docker.api.client.APIClient(
+        f"tcp://{host.IP}:{constants.DEFAULT_PORT}")
