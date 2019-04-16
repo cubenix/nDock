bbe580b35b25d985ffeb57babec46fcf4c0c3b74
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-04-16 11:11:47 +0530

    some refactoring

--- watchdock/app.py
+++ watchdock/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 from config_manager import ConfigManager
-import services.host as host
-import constants
+import services.renderer as renderer
 
 
 app = Flask(__name__)
@@ -13,15 +12,7 @@ config_manager = ConfigManager()
 @app.route('/home')
 @app.route('/index.html')
 def index():
-    hosts = config_manager.get_hosts()
-    host_containers = host.get_containers(hosts)
-
-    return render_template(
-        "index.html", title="Dashboard",
-        hosts=list(host_containers.keys()),
-        containers=list(host_containers.values()),
-        color_hosts=constants.COLOR_HOSTS,
-        color_background=constants.COLOR_BACKGROUND)
+    return renderer.render_dashboard(config_manager)
 
 
 if __name__ == '__main__':
---
+++ watchdock/services/renderer.py
@@ -0,0 +1,18 @@
+"""
+Module to handle requests and render required template.
+"""
+from flask import render_template
+import services.host as host
+import constants
+
+
+def render_dashboard(config_manager):
+    hosts = config_manager.get_hosts()
+    host_containers = host.get_containers(hosts)
+
+    return render_template(
+        "index.html", title="Dashboard",
+        hosts=list(host_containers.keys()),
+        containers=list(host_containers.values()),
+        color_hosts=constants.COLOR_HOSTS,
+        color_background=constants.COLOR_BACKGROUND)
