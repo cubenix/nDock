ce47185cb2c7ea7f66b65b82f0fa2402dfd5313a
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-05-09 07:15:46 +0530

    removed commented code

--- api/types/config.go
+++ api/types/config.go
@@ -1,16 +1,6 @@
 package types
 
-// Config represents the Json object in config.json
+// Config represents the JSON object in config.json
 type Config struct {
 	Hosts []Host
 }
-
-// // GetHosts returns Docker hosts read from the configuration
-// func (c *Config) GetHosts() []Host {
-// 	return c.hosts
-// }
-
-// // IntializeHosts returns Docker hosts read from the configuration
-// func (c *Config) IntializeHosts(hosts []Host) {
-// 	c.hosts = hosts
-// }
