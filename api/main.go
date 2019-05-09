5f45cba8d269352eadec121076f91bf77186528d
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-05-09 07:16:00 +0530

    optimized request-handler

--- api/main.go
+++ api/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gauravgahlot/watchdock/api/services"
 )
 
-var handler services.Handler
+var handler *services.Handler
 
 func init() {
 	var reader services.ConfigReader = services.JSONConfigReader{}
@@ -16,11 +16,11 @@ func init() {
 	if err != nil {
 		log.Fatalln("Failed to read the configuration")
 	}
-	handler = services.Handler{Hosts: &conf.Hosts}
+	handler = services.NewHandler(&conf.Hosts)
 }
 
 func main() {
-	http.HandleFunc("/hosts", handler.DockerHosts)
+	http.HandleFunc("/hosts", handler.Routes["hosts"])
 
 	// create the server and start listening
 	server := http.Server{
--- api/services/request-handler.go
+++ api/services/request-handler.go
@@ -7,16 +7,30 @@ import (
 	"github.com/gauravgahlot/watchdock/api/types"
 )
 
+type handleFunc func(w http.ResponseWriter, r *http.Request)
+
 // Handler represents struct with some data and request handlers for incoming HTTP requests
 type Handler struct {
-	Hosts *[]types.Host
+	Hosts  *[]types.Host
+	Routes map[string]handleFunc
 }
 
-// DockerHosts function handles the request for route /hosts
-func (h *Handler) DockerHosts(w http.ResponseWriter, r *http.Request) {
+// NewHandler initializes the request handler and returns a pointer to it
+func NewHandler(hosts *[]types.Host) *Handler {
+	h := Handler{Hosts: hosts}
+	h.initializeRoutes()
+	return &h
+}
+
+func (h *Handler) initializeRoutes() {
+	h.Routes = map[string]handleFunc{
+		"hosts": h.dockerHosts,
+	}
+}
+
+func (h *Handler) dockerHosts(w http.ResponseWriter, r *http.Request) {
 	for k, h := range *h.Hosts {
 		fmt.Printf("Hostname-%v: %v\n", k, h.Name)
 	}
-
 	w.Write([]byte("Docker hosts"))
 }
