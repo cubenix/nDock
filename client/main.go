7c9d870f4dedaecf35e8116f58ad08d179a1d2d4
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-05-11 23:57:20 +0530

    setup a plain gRPC client

--- client/main.go
+++ client/main.go
@@ -1,37 +1,65 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"log"
-	"net/http"
 
+	"github.com/gauravgahlot/watchdock/client/rpc"
 	"github.com/gauravgahlot/watchdock/client/services"
+	"github.com/gauravgahlot/watchdock/pb"
+
+	"google.golang.org/grpc"
 )
 
 const (
-	constAPIEndpoint    = "0.0.0.0:5000"
-	constClientEndpoint = "0.0.0.0:8080"
+	serverPort = ":5000"
+	clientPort = ":8080"
 )
 
 var (
-	apiEndpoint    = flag.String("api", constAPIEndpoint, "Endpoint for WatchDock API")
-	clientEndpoint = flag.String("client", constClientEndpoint, "Endpoint for WatchDock Client")
+	handler *services.Handler
+	clients *rpc.Clients
 )
 
-var handler *services.Handler
-
 func init() {
-	flag.Parse()
-	handler = services.NewHandler()
+	var reader services.ConfigReader = services.JSONConfigReader{}
+	conf, err := reader.ReadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed to read the configuration")
+	}
+	handler = services.NewHandler(&conf.Hosts)
 }
 
 func main() {
-	http.HandleFunc("/hosts", handler.Routes["hosts"])
-
-	// create the server and start listening
-	server := http.Server{
-		Addr: *clientEndpoint,
+	conn, err := grpc.Dial("localhost"+serverPort, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Println("Magic happens at:", *clientEndpoint)
-	log.Fatal(server.ListenAndServe())
+	defer conn.Close()
+	clients = rpc.InitializeClients(conn)
+	// server := http.Server{
+	// 	Addr: "localhost" + clientPort,
+	// }
+	// log.Println("Client App listening at port", clientPort)
+	// log.Fatal(server.ListenAndServe())
+
+	getContainersCount(clients.DockerService)
+	getContainer(clients.ContainerService)
+}
+
+func getContainersCount(client pb.DockerHostServiceClient) {
+	res, err := client.GetContainersCount(context.Background(), &pb.GetContainersCountRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(res.HostContainers)
+}
+
+func getContainer(client pb.ContainerServiceClient) {
+	res, err := client.GetContainer(context.Background(), &pb.GetContainerRequest{Id: "container-id"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(res)
 }
---
+++ client/rpc/rpc.go
@@ -0,0 +1,21 @@
+package rpc
+
+import (
+	"github.com/gauravgahlot/watchdock/pb"
+	"google.golang.org/grpc"
+)
+
+// Clients struct to hold all the service clients
+type Clients struct {
+	DockerService    pb.DockerHostServiceClient
+	ContainerService pb.ContainerServiceClient
+}
+
+// InitializeClients initializes client for respective service
+func InitializeClients(conn *grpc.ClientConn) *Clients {
+	c := Clients{
+		DockerService:    pb.NewDockerHostServiceClient(conn),
+		ContainerService: pb.NewContainerServiceClient(conn),
+	}
+	return &c
+}
