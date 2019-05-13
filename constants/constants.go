c08eb7c0fd061e6531c880dd2245b79e1c94af97
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-05-13 18:25:25 +0530

    adding few constants

---
+++ constants/constants.go
@@ -0,0 +1,58 @@
+package constants
+
+// BGClasses is a set background classes
+var BGClasses = [6]string{
+	bgPrimary,
+	bgSuccess,
+	bgInfo,
+	bgWarning,
+	bgDanger,
+	bgSecondary,
+}
+
+// BGCodes is a set background color codes
+var BGCodes = [6]string{
+	bgColor4e73df,
+	bgColor1cc88a,
+	bgColor36b9cc,
+	bgColorf6c23e,
+	bgColore74a3b,
+	bgColor858796,
+}
+
+// TextClasses is a set of text classes
+var TextClasses = [6]string{
+	textPrimary,
+	textSuccess,
+	textInfo,
+	textWarning,
+	textDanger,
+	textSecondary,
+}
+
+const (
+	bgPrimary   = "bg-primary"
+	bgSuccess   = "bg-success"
+	bgInfo      = "bg-info"
+	bgWarning   = "bg-warning"
+	bgDanger    = "bg-danger"
+	bgSecondary = "bg-secondary"
+)
+
+const (
+	bgColor4e73df = "#4e73df"
+	bgColor1cc88a = "#1cc88a"
+	bgColor36b9cc = "#36b9cc"
+	bgColorf6c23e = "#f6c23e"
+	bgColore74a3b = "#e74a3b"
+	bgColor858796 = "#858796"
+)
+
+const (
+	textPrimary   = "text-primary"
+	textSuccess   = "text-success"
+	textInfo      = "text-info"
+	textWarning   = "text-warning"
+	textDanger    = "text-danger"
+	textSecondary = "text-secondary"
+)
