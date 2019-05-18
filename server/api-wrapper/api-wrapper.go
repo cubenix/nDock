a3264676ee458f1bf53af8a09d6010fdb6965459
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-05-18 20:32:01 +0530

    fetching running containers from given host

--- server/api-wrapper/api-wrapper.go
+++ server/api-wrapper/api-wrapper.go
@@ -2,53 +2,91 @@ package apiwrapper
 
 import (
 	"context"
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/docker/docker/api/types"
-
 	"github.com/docker/docker/client"
+
 	"github.com/gauravgahlot/dockerdoodle/constants"
 )
 
+var (
+	apiClient *client.Client
+	doneCh    = make(chan struct{})
+)
+
+// InitializeClient initializes a new instance of the API Client
+func InitializeClient(host string) {
+	cli, err := client.NewClientWithOpts(client.WithHost(constants.DockerAPIProtocol+host+constants.DockerAPIPort), client.WithVersion(constants.DockerAPIVersion))
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiClient = cli
+}
+
 // GetContainersCount returns the number of containers running on a host
 func GetContainersCount(host string, all bool) (int32, error) {
-	cli, cliErr := client.NewClientWithOpts(client.WithHost(constants.DockerAPIProtocol+host+constants.DockerAPIPort), client.WithVersion(constants.DockerAPIVersion))
-	if cliErr != nil {
-		log.Fatal(cliErr)
-		return -1, cliErr
-	}
-
-	go containerStats(host)
-
-	c, err := cli.ContainerList(context.Background(), types.ContainerListOptions{Quiet: true, All: all})
+	c, err := GetContainers(context.Background(), true, all)
 	if err != nil {
 		log.Fatal(err)
 		return -1, err
 	}
-	return int32(len(c)), nil
+	defer apiClient.Close()
+	return int32(len(*c)), nil
 }
 
-func containerStats(host string) {
-	cli, _ := client.NewClientWithOpts(client.WithHost(constants.DockerAPIProtocol+host+constants.DockerAPIPort), client.WithVersion(constants.DockerAPIVersion))
-	containers, _ := cli.ContainerList(context.Background(), types.ContainerListOptions{Quiet: true})
-
-	go streamStats(cli, containers[0].ID)
-}
-
-func streamStats(cli *client.Client, id string) {
-	for {
-		s, e := cli.ContainerStats(context.Background(), id, true)
-		if e == io.EOF {
-			log.Println("EoF")
-		}
-		// if e != nil {
-		// 	break
-		// }
-		log.Println("OS Type: ", s.OSType)
-		var data []byte
-		s.Body.Read(data)
-		s.Body.Close()
-		log.Println(string(data))
+// GetContainers returns containers running on a host
+func GetContainers(ctx context.Context, quite bool, all bool) (*[]types.Container, error) {
+	c, err := apiClient.ContainerList(ctx, types.ContainerListOptions{Quiet: quite, All: all})
+	if err != nil {
+		log.Fatal(err)
+		return &[]types.Container{}, err
 	}
+	return &c, nil
+}
+
+func containerStats(ctx context.Context, cli *client.Client, host string) {
+	c, _ := cli.ContainerList(ctx, types.ContainerListOptions{Quiet: true})
+	defer log.Println("closing containerStats")
+
+	go getDockerStats(ctx, cli, c[0].ID)
+	time.Sleep(5 * time.Second)
+	doneCh <- struct{}{}
+}
+
+func getDockerStats(ctx context.Context, cli *client.Client, id string) {
+	defer log.Println("closing getDockerStats")
+	for {
+		select {
+		case <-doneCh:
+			log.Println("received DONE signal")
+			return
+		default:
+			getStats(ctx, cli, id)
+		}
+	}
+}
+
+func getStats(ctx context.Context, cli *client.Client, id string) {
+	s, e := cli.ContainerStats(ctx, id, false)
+	if e == io.EOF {
+		log.Println("EoF")
+	}
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer func() {
+		log.Println("closing Body")
+		s.Body.Close()
+	}()
+
+	d, _ := ioutil.ReadAll(s.Body)
+	var st types.Stats
+	json.Unmarshal(d, &st)
+	log.Println()
+	log.Println("CPU Usage", st.CPUStats.CPUUsage)
 }
--- server/services/docker-host.go
+++ server/services/docker-host.go
@@ -19,7 +19,7 @@ func (s *DockerHostService) GetContainersCount(ctx context.Context, req *pb.GetC
 
 	for _, host := range req.Hosts {
 		c := pb.HostContainerCount{Containers: make(map[string]int32)}
-
+		api.InitializeClient(host)
 		count, err := api.GetContainersCount(host, req.All)
 		if err != nil {
 			log.Fatal(err)
@@ -30,4 +30,24 @@ func (s *DockerHostService) GetContainersCount(ctx context.Context, req *pb.GetC
 		res.HostContainers = append(res.HostContainers, &c)
 	}
 	return &res, nil
+}
+
+// GetContainers returns ID and name of each container running on a host
+func (s *DockerHostService) GetContainers(ctx context.Context, req *pb.GetContainersRequest) (*pb.GetContainersResponse, error) {
+	api.InitializeClient(req.Host)
+	containers, err := api.GetContainers(context.Background(), false, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res := pb.GetContainersResponse{Containers: []*pb.Container{}}
+	for _, c := range *containers {
+		res.Containers = append(res.Containers, &pb.Container{Id: c.ID, Name: c.Names[0][1:]})
+	}
+	return &res, nil
+}
+
+// GetStats sends containers stats to the client via a stream
+func (s *DockerHostService) GetStats(req *pb.GetStatsRequest, stream pb.DockerHostService_GetStatsServer) error {
+	stream.Send(&pb.GetStatsReponse{})
+	return nil
 }
