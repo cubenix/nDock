804a443cd65b9ffc52540001d59e220e6c834be3
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-05-23 18:35:23 +0530

    getting details for a container

--- server/converter/converter.go
+++ server/converter/converter.go
@@ -9,41 +9,49 @@ import (
 func ToGetContainersResponse(containers *[]types.Container) *pb.GetContainersResponse {
 	res := pb.GetContainersResponse{Containers: []*pb.Container{}}
 	for _, c := range *containers {
-		pc := pb.Container{
-			Id:      c.ID,
-			Name:    c.Names[0][1:],
-			Image:   c.Image,
-			Command: c.Command,
-			Created: c.Created,
-			State:   c.State,
-			Status:  c.Status,
-			Ports:   []*pb.Port{},
-			Mounts:  []*pb.MountPoint{},
-		}
+		res.Containers = append(res.Containers, getContainer(&c))
+	}
+	return &res
+}
 
-		for _, p := range c.Ports {
-			port := pb.Port{
-				IP:          p.IP,
-				PrivatePort: int32(p.PrivatePort),
-				PublicPort:  int32(p.PublicPort),
-				Type:        p.Type,
-			}
-			pc.Ports = append(pc.Ports, &port)
-		}
+// ToGetContainerResponse returns response object for GetContainer call
+func ToGetContainerResponse(c *types.Container) *pb.GetContainerResponse {
+	return &pb.GetContainerResponse{Container: getContainer(c)}
+}
 
-		for _, m := range c.Mounts {
-			mp := pb.MountPoint{
-				Type:        string(m.Type),
-				Name:        m.Name,
-				Source:      m.Source,
-				Destination: m.Destination,
-				Mode:        m.Mode,
-				RW:          m.RW,
-			}
-			pc.Mounts = append(pc.Mounts, &mp)
-		}
-		res.Containers = append(res.Containers, &pc)
+func getContainer(c *types.Container) *pb.Container {
+	pc := pb.Container{
+		Id:      c.ID,
+		Name:    c.Names[0][1:],
+		Image:   c.Image,
+		Command: c.Command,
+		Created: c.Created,
+		State:   c.State,
+		Status:  c.Status,
+		Ports:   []*pb.Port{},
+		Mounts:  []*pb.MountPoint{},
 	}
 
-	return &res
+	for _, p := range c.Ports {
+		port := pb.Port{
+			IP:          p.IP,
+			PrivatePort: int32(p.PrivatePort),
+			PublicPort:  int32(p.PublicPort),
+			Type:        p.Type,
+		}
+		pc.Ports = append(pc.Ports, &port)
+	}
+
+	for _, m := range c.Mounts {
+		mp := pb.MountPoint{
+			Type:        string(m.Type),
+			Name:        m.Name,
+			Source:      m.Source,
+			Destination: m.Destination,
+			Mode:        m.Mode,
+			RW:          m.RW,
+		}
+		pc.Mounts = append(pc.Mounts, &mp)
+	}
+	return &pc
 }
--- server/services/container.go
+++ server/services/container.go
@@ -2,8 +2,13 @@ package services
 
 import (
 	"context"
+	"errors"
+	"log"
+	"strings"
 
 	"github.com/gauravgahlot/dockerdoodle/pb"
+	api "github.com/gauravgahlot/dockerdoodle/server/api-wrapper"
+	"github.com/gauravgahlot/dockerdoodle/server/converter"
 )
 
 // ContainerService is a gRPC service to serve requests for Docker containers
@@ -11,9 +16,18 @@ type ContainerService struct{}
 
 // GetContainer returns a container object for a container ID
 func (s *ContainerService) GetContainer(ctx context.Context, req *pb.GetContainerRequest) (*pb.GetContainerResponse, error) {
-	res := pb.GetContainerResponse{Container: &pb.Container{
-		Id:   req.Id,
-		Name: "container-name",
-	}}
-	return &res, nil
+	containers, err := api.GetContainers(req.Host, true, true)
+
+	if err != nil {
+		if err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+	}
+	for _, c := range *containers {
+		if strings.EqualFold(c.ID, req.ID) {
+			return converter.ToGetContainerResponse(&c), nil
+		}
+	}
+	return nil, errors.New("No container found with ID: " + req.ID)
 }
