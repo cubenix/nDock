376b8500024b204b1f01070c82434c27e73af7dc
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-05-24 11:41:42 +0530

    redirecting from "more" to /host/containers/

--- client/controller/container.go
+++ client/controller/container.go
@@ -1,15 +1,10 @@
 package controller
 
 import (
-	"context"
 	"html/template"
-	"log"
 	"net/http"
-	"strings"
 
-	"github.com/gauravgahlot/dockerdoodle/client/helpers"
 	"github.com/gauravgahlot/dockerdoodle/client/rpc"
-	"github.com/gauravgahlot/dockerdoodle/client/viewmodels"
 	"github.com/gauravgahlot/dockerdoodle/types"
 )
 
@@ -24,23 +19,6 @@ func (c container) registerRoutes() {
 }
 
 func (c container) handleContainerDetails(w http.ResponseWriter, r *http.Request) {
-	ctx := viewmodels.ContainerDetails{SelectedHost: r.FormValue("host")}
-	ctx.Title = "Container Details"
-	ctx.Hosts = []viewmodels.Host{}
-
-	var hostIP string
-	for _, h := range *c.hosts {
-		ctx.Hosts = append(ctx.Hosts, viewmodels.Host{Name: h.Name, IP: h.IP})
-		if strings.EqualFold(h.Name, r.FormValue("host")) {
-			hostIP = h.IP
-			break
-		}
-	}
-	res, err := helpers.GetContainer(context.Background(), c.client, hostIP, r.FormValue("containerID"))
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusNotFound)
-	}
-	ctx.Container = *res
-	c.containerTemplate.Execute(w, ctx)
+	host := r.FormValue("host")
+	http.Redirect(w, r, "/host/containers/"+host, http.StatusPermanentRedirect)
 }
